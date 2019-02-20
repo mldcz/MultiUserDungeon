631205b85748561f34a147c413ee386d9bc10608
Unknown <alienor.fleury88@gmail.com>
2019-02-20 16:56:03 +0100

    MAJ Joueur

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,5 +1,12 @@
 public class Joueur extends Combattant {
 	
+	public String getNomJ() {
+		return nomJ;
+	}
+
+	public void setNomJ(String nomJ) {
+		this.nomJ = nomJ;
+	}
 	private String nomJ;
 	
 	public Joueur (String nomJ) {
