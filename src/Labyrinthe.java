d014cf3e0510a61c8e14e163c1d939cb6a351852
Aliénor Fleury <alienor.fleury88@gmail.com>
2019-03-06 14:24:14 +0100

    MAJ Labyrinthe

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -22,29 +22,29 @@ public class Labyrinthe {
 		
 		tabPiece[1][1]="N,E";
 		tabPiece[1][2]="O,E";
-		tabPiece[1][3]="O,N,E,M";
+		tabPiece[1][3]="O,N,E";
 		tabPiece[1][4]="O,N,E";
-		tabPiece[1][5]="O,N,M";
+		tabPiece[1][5]="O,N";
 		tabPiece[2][1]="N,E,S";
-		tabPiece[2][2]="O,M";
+		tabPiece[2][2]="O";
 		tabPiece[2][3]="N,E,S";
-		tabPiece[2][4]="O,N,S,M";
-		tabPiece[2][5]="S,M";
+		tabPiece[2][4]="O,N,S";
+		tabPiece[2][5]="S";
 		tabPiece[3][1]="O,E,S";
-		tabPiece[3][2]="O,N,E,M";
-		tabPiece[3][3]="O,S,M";
+		tabPiece[3][2]="O,N,E";
+		tabPiece[3][3]="O,S";
 		tabPiece[3][4]="N,E,S";
-		tabPiece[3][5]="O,N,M";
-		tabPiece[4][1]="N,S,M";
+		tabPiece[3][5]="O,N";
+		tabPiece[4][1]="N,S";
 		tabPiece[4][2]="E,S";
-		tabPiece[4][3]="O,N,M";
-		tabPiece[4][4]="N,S,T";
-		tabPiece[4][5]="N,S,M";
+		tabPiece[4][3]="O,N";
+		tabPiece[4][4]="N,S";
+		tabPiece[4][5]="N,S";
 		tabPiece[5][1]="E,S";
 		tabPiece[5][2]="O,M";
 		tabPiece[5][3]="E,S";
-		tabPiece[5][4]="O,E,S,M";
-		tabPiece[5][5]="O,S,M";
+		tabPiece[5][4]="O,E,S";
+		tabPiece[5][5]="O,S";
 
 		
 		    
