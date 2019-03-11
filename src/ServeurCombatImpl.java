47c31ea0680c408e0f68d207a969f623046b1095
afleury88 <34154023+afleury88@users.noreply.github.com>
2019-03-11 18:46:08 +0100

    Create ServeurCombatImpl.java

---
+++ src/ServeurCombatImpl.java
@@ -0,0 +1,25 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
+
+import Combattant;
+
+public class ServerCombatImpl  extends UnicastRemoteObject implements ServerCombatInterface{
+
+    private static final long serialVersionUID = 1L;
+    private Combattant c1;
+    private Combattant c2;
+
+    public ServerFightImpl() throws RemoteException {
+        super();
+    }
+
+    public void initialisationCombat(Combattant c1, Combattant c1)  throws RemoteException {
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+
+    public Combattant turn() throws RemoteException{
+        return new Combat(c1, c2).start();
+    }
+}
