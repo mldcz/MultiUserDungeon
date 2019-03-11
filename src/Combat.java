48a4c6daddaa4cf9928fae3d82b4c24e258f3b42
afleury88 <34154023+afleury88@users.noreply.github.com>
2019-03-11 18:45:30 +0100

    Combat.java

---
+++ src/Combat.java
@@ -0,0 +1,15 @@
+import Combattant;
+
+public class Combat {
+    private Combattant c1;
+    private Combattant c2;
+
+    public Combat(Combattant c1, Combattant c2) {
+        this.c1 = c1;
+        this.c2 = c2;
+
+        //combat avec pv qui changent
+
+    }
+
+}
