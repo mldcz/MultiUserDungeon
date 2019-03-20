2880f97c60d1042b087637f69b38830078f8244e
Aliénor Fleury <alienor.fleury88@gmail.com>
2019-03-20 17:41:44 +0100

    MAJ InterfaceServeurPrincipal

--- src/InterfaceServeurPrincipal.java
+++ src/InterfaceServeurPrincipal.java
@@ -10,22 +10,7 @@ public interface InterfaceServeurPrincipal extends java.rmi.Remote
 	public synchronized void supprimerJoueur(Client client) throws java.rmi.RemoteException;
 	public synchronized boolean interrogeDisponibilite() throws RemoteException;
 	public synchronized void authentification() throws RemoteException;
-	public synchronized String recupererInformations() throws RemoteException;
-
-
-	public String affichageAuthentification() throws throws java.rmi.RemoteException;
-	public boolean interrogeDisponibilite() throws java.rmi.RemoteException;
-	//public void creationLab() throws java.rmi.RemoteException;
-	public void changerPiece() throws java.rmi.RemoteException;
-	public boolean isLabFini() throws java.rmi.RemoteException;
-	public boolean exitLab() throws java.rmi.RemoteException;
-	public String getListJoueur()throws java.rmi.RemoteException;
-	//public void generationPiece(String tabPiece[][]) throws RemoteException;
-	//public void authentification() throws java.rmi.RemoteException;
-	// public String recupererInformations() throws java.rmi.RemoteException;
-	//public void desinscription() throws java.rmi.RemoteException;
-	//public String ToString() throws java.rmi.RemoteException;
-	// public Position position() throws java.rmi.RemoteException;
-	//public void verification() throws java.rmi.RemoteException;
-	//public String getInformations() throws java.rmi.RemoteException;
+	public synchronized void desinscription(Client client) throws RemoteException;
+	public void exitLab(Joueur joueur) throws RemoteException;
+	public String getListJoueur() throws RemoteException;
 }
